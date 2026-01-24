c4fa6c1d72b18b36340744f1f9b6295dbe78fdb5
Anya Jiang <anya@whsrobotics.org>
2026-01-23 23:43:10 -0500

    2022 pykit hood files

---
+++ src/subsystems/hood/hoodsubsystemio.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from pykit.autolog import autolog
+
+
+class HoodSubsystemIO:
+    @autolog
+    @dataclass
+    class HoodSubsystemIOInputs:
+        hoodConnected: bool = False
+
+        hoodPosition: float = 0.0
+        hoodSpeed: float = 0.0
+
+        hoodAppliedVolts: float = 0.0
+        hoodSupplyAmps: float = 0.0
+
+    def updateInputs(self, inputs: HoodSubsystemIOInputs):
+        pass
+
+    def set_hood_position(self, position: float):
+        # Code to set the hood position in radians
+        pass
+
+    def set_hood_volts(self, volts: float):
+        # Code to set the hood voltage in volts
+        pass
