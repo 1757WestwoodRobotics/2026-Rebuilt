b793e9a0b9ada3ea0031871ca8b09d6803c4d7d5
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-02-24 16:02:42 -0500

    constants fix, sysid

--- src/constants/intake.py
+++ src/constants/intake.py
@@ -3,13 +3,15 @@ from wpimath.trajectory import TrapezoidProfileRadians
 from wpimath.geometry import Rotation2d
 from wpimath.system.plant import DCMotor
 
+from .math import kKilogramToLbs, kMetersPerInch
+
 kPivotMotor = DCMotor.krakenX60(1)
 
-kPivotMass = 2.0  # kg
-kPivotGearRatio = 250.0  # dimensionless
-kPivotArmLength = 1.0  # m
+kPivotMass = 7 * kKilogramToLbs  # kg
+kPivotGearRatio = 64 / 1  # dimensionless
+kPivotArmLength = 4.843 * kMetersPerInch  # meters
 
-kPivotCANId = 17
+kPivotCANId = 1
 
 kPivotKp = 210.68
 kPivotKi = 0
@@ -36,13 +38,13 @@ kPivotCurrentLimit = (
     .with_supply_current_limit_enable(True)
 )
 
-kRollerMotor = DCMotor.falcon500(1)
-kRollerGearRatio = 1.0
+kRollerMotor = DCMotor.krakenX60FOC(1)
+kRollerGearRatio = 18 / 24
 
 kPivotRetractedPosition = Rotation2d()
 kPivotExtendedPosition = Rotation2d.fromDegrees(90)
 
-kRollerCANId = 18
+kRollerCANId = 2
 
 kRollerForwardVoltage = 6.0
 kRollerReverseVoltage = -6.0
