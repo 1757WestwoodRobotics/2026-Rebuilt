720882eed72d9230c6b32a010ff242068360677b
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-02-24 15:47:46 -0500

    fix inputs issues

--- src/subsystems/intake/intakesubsystemio.py
+++ src/subsystems/intake/intakesubsystemio.py
@@ -14,6 +14,9 @@ class IntakeSubsystemIO:
         pivotPosition: float = 0  # rad
         pivotVelocity: float = 0  # rad / s
 
+        pivotAppliedVolts: float = 0
+        pivotSupplyAmps: float = 0
+
         rollerPosition: float = 0  # rad
         rollerVelocity: float = 0  # rad / s
 
