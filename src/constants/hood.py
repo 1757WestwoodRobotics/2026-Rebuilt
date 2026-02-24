87b9237340579859d8cc198d3f3732b62d7092ae
Anya Jiang <anya@whsrobotics.org>
2026-02-23 19:41:27 -0500

    constants structure

---
+++ src/constants/hood.py
@@ -0,0 +1,10 @@
+from phoenix6.configs.talon_fx_configs import CurrentLimitsConfigs
+from wpimath.geometry import Rotation2d, Rotation3d, Translation3d, Transform3d
+from wpimath.system.plant import DCMotor
+
+kHoodMinAngle = Rotation2d.fromDegrees(45) # from vertical to the right
+kHoodMaxAngle = Rotation2d.fromDegrees(82)
+
+kHoodGearRatio = 
+
+kTurretCanId = 21 
