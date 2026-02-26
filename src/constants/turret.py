9c229dfbd60e9e7e9d1d85091b81724542d2db47
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-02-26 14:01:58 -0500

    corrected constants

--- src/constants/turret.py
+++ src/constants/turret.py
@@ -2,16 +2,17 @@ from phoenix6.configs.talon_fx_configs import CurrentLimitsConfigs
 from wpimath.geometry import Rotation2d, Rotation3d, Translation3d, Transform3d
 from wpimath.system.plant import DCMotor
 
-kTurretMinAngle = Rotation2d.fromDegrees(-160)
-kTurretMaxAngle = Rotation2d.fromDegrees(160)
+kTurretStartingAngle = Rotation2d.fromDegrees(90)
+kTurretMinAngle = Rotation2d.fromDegrees(0)
+kTurretMaxAngle = Rotation2d.fromDegrees(270)
 
-kTurretGearRatio = (1 / 5) * (13 / 360)
+kTurretGearRatio = (50 / 12) * (80 / 15)
 
 kTurretMaxVelocity = Rotation2d.fromDegrees(180)  # per second
 kTurretMaxAcceleration = Rotation2d.fromDegrees(360)  # per second squared
 kTurretTolerance = Rotation2d.fromDegrees(1.0)
 
-kTurretCanId = 22
+kTurretCanId = 9
 kTurretPGain = 0.1
 kTurretIGain = 0.0
 kTurretDGain = 0.0
@@ -25,7 +26,7 @@ kTurretCurrentLimit = (
     .with_supply_current_limit_enable(True)
 )
 
-kTurretSimMotor = DCMotor.falcon500(1)
+kTurretSimMotor = DCMotor.krakenX44FOC(1)
 kTurretSimInertia = 0.005  # kg m^2
 
 kTurretLocation = Transform3d(
