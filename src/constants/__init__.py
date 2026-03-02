e20451b47ba8e1e9b028ba5c9c3f0fd197518b0b
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-03-02 10:29:28 -0500

    document conceptual change to constants

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,7 +1,7 @@
 """
 
 The constants module is a convenience place for teams to hold robot-wide
-numerical or boolean constants. Don't use this for any other purpose!
+constant values. Don't use this for any other purpose!
 
 Physical constants must have their units specified
 Default units:
