5f0847201fca88e3bfa4d2d097209abb17666b1f
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-03-02 10:37:06 -0500

    document change in angles

--- src/constants/intake.py
+++ src/constants/intake.py
@@ -31,7 +31,7 @@ kPivotConstraints = TrapezoidProfileRadians.Constraints(
     kPivotMaxVelocity, kPivotMaxAcceleration
 )
 
-# Positive angle is extended, negative angle is retracted
+# Positive angle is upwards, 0 angle is horizontal, and negative angle is downwards
 kPivotMinAngle = Rotation2d()
 kPivotMaxAngle = Rotation2d.fromDegrees(125.5)
 kPivotStartAngle = Rotation2d.fromDegrees(125.5)
