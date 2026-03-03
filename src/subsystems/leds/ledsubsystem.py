2276dbb882428beabd4287f814dc576116fa8d80
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-03-03 10:13:22 -0500

    document why no io model

--- src/subsystems/leds/ledsubsystem.py
+++ src/subsystems/leds/ledsubsystem.py
@@ -11,9 +11,16 @@ from constants.led import (
 )
 from constants.drive import kCANivoreName
 from robotstate import RobotState
+from util.logtracer import LogTracer
 
 
 class LEDSubsystem(Subsystem):
+    """
+    LED subsystem has no real simulated instance, so we are initializing it no matter what.
+    If we wanted to be more correct we could make a simulated CANdle that just logs the set control,
+    but it doesn't seem worth the effort at this time.
+    """
+
     def __init__(self) -> None:
         Subsystem.__init__(self)
         self.setName(type(self).__name__)
@@ -21,6 +28,7 @@ class LEDSubsystem(Subsystem):
         self.candle = CANdle(kCANdleCANId, kCANivoreName)
 
     def periodic(self) -> None:
+        LogTracer.resetOuter("LEDSubsystem.periodic")
         if DriverStation.isEStopped():
             self.candle.set_control(kEstopAnim)
         elif DriverStation.isDisabled():
@@ -31,3 +39,4 @@ class LEDSubsystem(Subsystem):
                 self.candle.set_control(kPrepFlashAnim)
             else:
                 self.candle.set_control(kPrepAnim)
+        LogTracer.recordTotal()
