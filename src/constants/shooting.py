4d5ce37e25dc2592e95e8ce6c61ff1cb26bb9d39
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-03-03 17:26:13 -0500

    more explicit shooting map

---
+++ src/constants/shooting.py
@@ -0,0 +1,16 @@
+"""
+This module is for all the functions related to higher order shooting operations
+This should include any mapping functions for distance,
+compensation factors for shooting on the move, and any other functions that are used to calculate
+the parameters for shooting commands, such as flywheel speed or hood angle, based on various inputs
+such as distance to target, robot velocity, etc.
+"""
+
+from functools import partial
+
+from numpy import interp
+
+kShootingMap = partial(interp, xp=[0, 5, 10, 15], fp=[0, 1000, 2000, 3000])
+# Example mapping function for distance to flywheel speed,
+# where xp is the distance in meters and fp is the
+# corresponding flywheel speed in rad/s
