93ba27953dd9b8db1d57bf0c2a2b1488b0cbd20f
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-03-05 18:50:31 -0500

    document color

--- src/constants/led.py
+++ src/constants/led.py
@@ -14,16 +14,20 @@ kEmpty0Anim = EmptyAnimation(0)
 kEmpty1Anim = EmptyAnimation(1)
 
 kDisabledAnim = RainbowAnimation(0, kCANdleOnboardLedCount + kCANdleExternalLedCount, 0)
+
+# red
 kEstopAnim = StrobeAnimation(
     0, kCANdleOnboardLedCount + kCANdleExternalLedCount, 0, RGBWColor(255, 0, 0), 10
 )
 
+# yellow
 kPrepColor = RGBWColor(255, 208, 0)
 kPrepAnim = SolidColor(0, kCANdleOnboardLedCount + kCANdleExternalLedCount, kPrepColor)
 kPrepFlashAnim = StrobeAnimation(
     0, kCANdleOnboardLedCount + kCANdleExternalLedCount, 0, kPrepColor, 10
 )
 
+# green
 kShootingColor = RGBWColor(0, 255, 0)
 kShootingAnim = SolidColor(
     0, kCANdleOnboardLedCount + kCANdleExternalLedCount, kShootingColor
