70135d798dc049dc4a4c70debfcc866c7e3fe622
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-03-08 14:58:02 -0400

    shooting maps

--- src/constants/shooting.py
+++ src/constants/shooting.py
@@ -11,13 +11,21 @@ from functools import partial
 from numpy import interp
 from wpimath.geometry import Rotation2d
 
-kShootingMap = partial(interp, xp=[0, 5, 10, 15], fp=[0, 1000, 2000, 3000])
+kShootingMap = partial(
+    interp,
+    xp=[1.39, 1.86, 2.08, 2.26, 2.86, 3.12, 3.45],
+    fp=[200, 230, 230, 230, 245, 250, 260],
+)
 # Example mapping function for distance to flywheel speed,
 # where xp is the distance in meters and fp is the
 # corresponding flywheel speed in rad/s
 
 kHoodAngleMap = lambda x: Rotation2d.fromDegrees(
-    interp(x, xp=[0, 5, 10, 15], fp=[0, 15, 30, 45])
+    interp(
+        x,
+        xp=[1.39, 1.86, 2.08, 2.26, 2.86, 3.12, 3.45],
+        fp=[12, 15, 17.5, 20, 21, 23, 24],
+    )
 )
 
 kFeedFlywheelMap = partial(interp, xp=[0, 5, 10], fp=[5, 40, 80])  # rad / s
