47730bd8f7f3fa70402a5f32a0deb71f447c8582
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-03-12 21:45:32 -0400

    technically better explicit

--- src/robotcontainer.py
+++ src/robotcontainer.py
@@ -436,8 +436,10 @@ class RobotContainer:
         )
 
         self.oi.driverController.povDown().onTrue(
-            ResetGyro(self.drive, Pose2d(0, 0, 0))
-        ).onTrue(self.oi.rumbleControllersCommand().withTimeout(0.5))
+            ResetGyro(self.drive, Pose2d(0, 0, 0)).andThen(
+                self.oi.rumbleControllersCommand().withTimeout(0.5)
+            )
+        )
 
         self.oi.driverController.x().whileTrue(DefenseState(self.drive))
 
