7ee5f81d34792a71055d1250eab9bf613e9dc33d
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-03-14 12:06:31 -0400

    turret override

--- src/commands/overridecommands.py
+++ src/commands/overridecommands.py
@@ -3,11 +3,14 @@ from pykit.networktables.loggednetworknumber import LoggedNetworkNumber
 from wpimath.geometry import Rotation2d
 from commands.flywheelcommands import fireAtSpeed
 from commands.hoodcommands import angleHood
+from commands.turretcommands import angleTurret
 from subsystems.flywheel.flywheelsubsystem import FlywheelSubsystem
 from subsystems.hood.hoodsubsystem import HoodSubsystem
+from subsystems.turret.turretsubsystem import TurretSubsystem
 
 _flywheelSetpoint = LoggedNetworkNumber("Flywheel/Setpoint", 0.0)
 _hoodSetpoint = LoggedNetworkNumber("Hood/Setpoint", 0.0)
+_turretSetpoint = LoggedNetworkNumber("Turret/Setpoint", 0.0)
 
 
 def overrideFlywheel(flywheel: FlywheelSubsystem) -> Command:
@@ -28,3 +31,12 @@ def overrideHood(hood: HoodSubsystem) -> Command:
     return angleHood(
         hood, lambda: Rotation2d.fromDegrees(_hoodSetpoint.value)
     ).withName("OverrideHood")
+
+
+def overrideTurret(turret: TurretSubsystem) -> Command:
+    """
+    Returns a command that allows the operator to override the turret angle using network input
+    """
+    return angleTurret(
+        turret, lambda: Rotation2d.fromDegrees(_turretSetpoint.value)
+    ).withName("OverrideTurret")
